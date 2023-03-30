097e4a5aa7e780a0c13ebb7e44fa4b81b1a34484
booky10 <boooky10@gmail.com>
2023-03-30 14:35:05 +0200

    Fix minecraft classes being loaded before mixins are applied
    Resulted in some mods (like FerriteCore) crashing

--- src/main/java/dev/booky/stackdeobf/compat/CompatUtil.java
+++ src/main/java/dev/booky/stackdeobf/compat/CompatUtil.java
@@ -1,12 +1,9 @@
 package dev.booky.stackdeobf.compat;
 // Created by booky10 in StackDeobfuscator (21:20 29.03.23)
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import dev.booky.stackdeobf.StackDeobfMod;
-import net.minecraft.DetectedVersion;
-import net.minecraft.SharedConstants;
-import net.minecraft.Util;
-import net.minecraft.util.GsonHelper;
 import org.apache.logging.log4j.LogManager;
 import org.slf4j.LoggerFactory;
 
@@ -18,62 +15,39 @@ import java.util.Objects;
 
 public final class CompatUtil {
 
+    private static final JsonObject VERSION_DATA;
+
     static {
-        try {
-            // mixin plugin is loaded before the version info is loaded,
-            // load it manually
-            SharedConstants.tryDetectVersion();
-        } catch (NoSuchMethodError ignored) {
-            // doesn't exist in old versions
-        }
-    }
-
-    public static final String VERSION_ID = Util.make(() -> {
-        try {
-            return SharedConstants.getCurrentVersion().getId();
-        } catch (NoSuchMethodError error) {
-            // version older than 1.19.4, manually read id from version.json
-            return getVersionJson().get("id").getAsString();
-        }
-    });
-
-    public static final String VERSION_NAME = Util.make(() -> {
-        try {
-            return SharedConstants.getCurrentVersion().getName();
-        } catch (NoSuchMethodError error) {
-            // version older than 1.19.4, manually read id from version.json
-            return getVersionJson().get("name").getAsString();
-        }
-    });
-
-    public static final int WORLD_VERSION = Util.make(() -> {
-        try {
-            return SharedConstants.getCurrentVersion().getDataVersion().getVersion();
-        } catch (NoSuchMethodError error) {
-            // version older than 1.19.4, manually read id from version.json
-            return getVersionJson().get("world_version").getAsInt();
-        }
-    });
-
-    public static final ILogger LOGGER = Util.make(() -> {
-        try {
-            // versions below 22w03a don't have mojangs logging library and therefore no slf4j
-            Class.forName("org.slf4j.Logger");
-            return new Slf4jLogger(LoggerFactory.getLogger(StackDeobfMod.class));
-        } catch (ClassNotFoundException exception) {
-            return new Log4jLogger(LogManager.getLogger(StackDeobfMod.class));
-        }
-    });
-
-    private CompatUtil() {
-    }
-
-    private static JsonObject getVersionJson() {
-        try (InputStream input = DetectedVersion.class.getResourceAsStream("/version.json");
+        try (InputStream input = CompatUtil.class.getResourceAsStream("/version.json");
              Reader reader = new InputStreamReader(Objects.requireNonNull(input, "version.json not found"))) {
-            return GsonHelper.parse(reader);
+            // can't use mojangs utility class GsonHelper, this would load ResourceLocation which breaks some
+            // mods injecting into that class, this class is loaded before any mixins are applied
+            VERSION_DATA = new Gson().fromJson(reader, JsonObject.class);
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+    }
+
+    // get data directly from version.json, loading SharedConstants would result
+    // in classes being loaded before mixins are applied
+    public static final String VERSION_ID = VERSION_DATA.get("id").getAsString();
+    public static final String VERSION_NAME = VERSION_DATA.get("name").getAsString();
+    public static final int WORLD_VERSION = VERSION_DATA.get("world_version").getAsInt();
+
+    public static final ILogger LOGGER;
+
+    static {
+        ILogger logger;
+        try {
+            // versions below 22w03a don't have mojangs logging library and therefore no slf4j
+            Class.forName("org.slf4j.Logger");
+            logger = new Slf4jLogger(LoggerFactory.getLogger(StackDeobfMod.class));
+        } catch (ClassNotFoundException exception) {
+            logger = new Log4jLogger(LogManager.getLogger(StackDeobfMod.class));
+        }
+        LOGGER = logger;
+    }
+
+    private CompatUtil() {
     }
 }

fbd32cfd76efd3b70a76441fbbe62afb3fec7ef0
booky10 <boooky10@gmail.com>
2023-07-30 04:10:04 +0200

    Add static links for mojang experiment profile containers

--- common/src/main/java/dev/booky/stackdeobf/mappings/providers/MojangMappingProvider.java
+++ common/src/main/java/dev/booky/stackdeobf/mappings/providers/MojangMappingProvider.java
@@ -26,12 +26,33 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class MojangMappingProvider extends AbstractMappingProvider {
+
+    // taken from https://github.com/PrismLauncher/meta/blob/63194d47e829a8c072e5a40dac37863a21d1b11d/static/mojang/minecraft-experiments.json
+    private static final Map<String, VerifiableUrl> STATIC_MINECRAFT_EXPERIMENTS = Map.ofEntries(
+            Map.entry("1.19_deep_dark_experimental_snapshot-1", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/b1e589c1d6ed73519797214bc796e53f5429ac46/1_19_deep_dark_experimental_snapshot-1.zip"), VerifiableUrl.HashType.SHA1, "b1e589c1d6ed73519797214bc796e53f5429ac46")),
+            Map.entry("1.18_experimental-snapshot-7", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/ab4ecebb133f56dd4c4c4c3257f030a947ddea84/1_18_experimental-snapshot-7.zip"), VerifiableUrl.HashType.SHA1, "ab4ecebb133f56dd4c4c4c3257f030a947ddea84")),
+            Map.entry("1.18_experimental-snapshot-6", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/4697c84c6a347d0b8766759d5b00bc5a00b1b858/1_18_experimental-snapshot-6.zip"), VerifiableUrl.HashType.SHA1, "4697c84c6a347d0b8766759d5b00bc5a00b1b858")),
+            Map.entry("1.18_experimental-snapshot-5", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/d9cb7f6fb4e440862adfb40a385d83e3f8d154db/1_18_experimental-snapshot-5.zip"), VerifiableUrl.HashType.SHA1, "d9cb7f6fb4e440862adfb40a385d83e3f8d154db")),
+            Map.entry("1.18_experimental-snapshot-4", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/b92a360cbae2eb896a62964ad8c06c3493b6c390/1_18_experimental-snapshot-4.zip"), VerifiableUrl.HashType.SHA1, "b92a360cbae2eb896a62964ad8c06c3493b6c390")),
+            Map.entry("1.18_experimental-snapshot-3", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/846648ff9fe60310d584061261de43010e5c722b/1_18_experimental-snapshot-3.zip"), VerifiableUrl.HashType.SHA1, "846648ff9fe60310d584061261de43010e5c722b")),
+            Map.entry("1.18_experimental-snapshot-2", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/0adfe4f321aa45248fc88ac888bed5556633e7fb/1_18_experimental-snapshot-2.zip"), VerifiableUrl.HashType.SHA1, "0adfe4f321aa45248fc88ac888bed5556633e7fb")),
+            Map.entry("1.18_experimental-snapshot-1", new VerifiableUrl(URI.create("https://launcher.mojang.com/v1/objects/231bba2a21e18b8c60976e1f6110c053b7b93226/1_18_experimental-snapshot-1.zip"), VerifiableUrl.HashType.SHA1, "231bba2a21e18b8c60976e1f6110c053b7b93226")),
+            Map.entry("1.16_combat-3", new VerifiableUrl(URI.create("https://launcher.mojang.com/experiments/combat/2557b99d95588505e988886220779087d7d6b1e9/1_16_combat-3.zip"), VerifiableUrl.HashType.SHA1, "2557b99d95588505e988886220779087d7d6b1e9")),
+            Map.entry("1.16_combat-0", new VerifiableUrl(URI.create("https://launcher.mojang.com/experiments/combat/5a8ceec8681ed96ab6ecb9607fb5d19c8a755559/1_16_combat-0.zip"), VerifiableUrl.HashType.SHA1, "5a8ceec8681ed96ab6ecb9607fb5d19c8a755559")),
+            Map.entry("1.15_combat-6", new VerifiableUrl(URI.create("https://launcher.mojang.com/experiments/combat/52263d42a626b40c947e523128f7a195ec5af76a/1_15_combat-6.zip"), VerifiableUrl.HashType.SHA1, "52263d42a626b40c947e523128f7a195ec5af76a")),
+            Map.entry("1.15_combat-1", new VerifiableUrl(URI.create("https://launcher.mojang.com/experiments/combat/ac11ea96f3bb2fa2b9b76ab1d20cacb1b1f7ef60/1_15_combat-1.zip"), VerifiableUrl.HashType.SHA1, "ac11ea96f3bb2fa2b9b76ab1d20cacb1b1f7ef60")),
+            Map.entry("1.14_combat-3", new VerifiableUrl(URI.create("https://launcher.mojang.com/experiments/combat/0f209c9c84b81c7d4c88b4632155b9ae550beb89/1_14_combat-3.zip"), VerifiableUrl.HashType.SHA1, "0f209c9c84b81c7d4c88b4632155b9ae550beb89")),
+            Map.entry("1.14_combat-0", new VerifiableUrl(URI.create("https://launcher.mojang.com/experiments/combat/d164bb6ecc5fca9ac02878c85f11befae61ac1ca/1_14_combat-0.zip"), VerifiableUrl.HashType.SHA1, "d164bb6ecc5fca9ac02878c85f11befae61ac1ca")),
+            Map.entry("1.14.3 - Combat Test / 5d5e0be06e714f03bba436c42db4c85b", new VerifiableUrl(URI.create("https://launcher.mojang.com/experiments/combat/610f5c9874ba8926d5ae1bcce647e5f0e6e7c889/1_14_combat-212796.zip"), VerifiableUrl.HashType.SHA1, "610f5c9874ba8926d5ae1bcce647e5f0e6e7c889")));
 
     private static final String LICENSE = """
             (c) 2020 Microsoft Corporation.These mappings are provided "as-is" and you bear the risk of using them.
@@ -91,6 +112,42 @@ public class MojangMappingProvider extends AbstractMappingProvider {
     }
 
     private CompletableFuture<VerifiableUrl> fetchMojangMappingsUri(String mcVersion, Executor executor) {
+        // some versions are not present in official version manifest, so use these hard-coded links
+        VerifiableUrl staticUrl = STATIC_MINECRAFT_EXPERIMENTS.get(mcVersion);
+        if (staticUrl != null) {
+            LOGGER.warn("Static url found for {}, using {} for downloading", mcVersion, staticUrl.getUrl());
+            return staticUrl.get(executor).thenApply(profileContainerZip -> {
+                JsonObject infoObj = null;
+                try (ByteArrayInputStream input = new ByteArrayInputStream(profileContainerZip);
+                     ZipInputStream zipInput = new ZipInputStream(input)) {
+                    ZipEntry entry;
+                    while ((entry = zipInput.getNextEntry()) != null) {
+                        if (!entry.getName().endsWith(".json")) {
+                            continue;
+                        }
+
+                        try (Reader reader = new InputStreamReader(zipInput)) {
+                            infoObj = GSON.fromJson(reader, JsonObject.class);
+                        }
+                        break;
+                    }
+                } catch (IOException exception) {
+                    throw new RuntimeException(exception);
+                }
+
+                if (infoObj == null) {
+                    throw new IllegalStateException("No version metadata found in static profile for " + mcVersion);
+                }
+
+                JsonObject mappingsData = infoObj
+                        .getAsJsonObject("downloads")
+                        .getAsJsonObject(this.environment + "_mappings");
+                URI mappingsUrl = URI.create(mappingsData.get("url").getAsString());
+                String mappingsSha1 = mappingsData.get("sha1").getAsString();
+                return new VerifiableUrl(mappingsUrl, VerifiableUrl.HashType.SHA1, mappingsSha1);
+            });
+        }
+
         URI manifestUri = URI.create(System.getProperty("stackdeobf.manifest-uri",
                 "https://piston-meta.mojang.com/mc/game/version_manifest_v2.json"));
 

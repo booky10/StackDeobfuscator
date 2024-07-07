25b29de267fecb5b79741d9ce26b14b6cd54bed7
booky10 <boooky10@gmail.com>
2024-07-07 17:49:40 +0200

    Add fabric metadata to common jar
    Improves look of jar-in-jar'd common module in modmenu

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,3 +4,10 @@ dependencies {
     // only int -> object maps required for fastutil
     compileOnly(libs.bundles.builtin)
 }
+
+tasks.processResources {
+    inputs.property("version", project.version)
+    filesMatching("fabric.mod.json") {
+        expand("version" to project.version)
+    }
+}
